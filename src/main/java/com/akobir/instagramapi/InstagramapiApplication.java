328a21960c3aeefc8a66b09032decec246098983
AkobirToshtemirov <toshtemirovakobir8@gmail.com>
2024-02-17 19:44:43 +0500

    initial commit

---
+++ src/main/java/com/akobir/instagramapi/InstagramapiApplication.java
@@ -0,0 +1,13 @@
+package com.akobir.instagramapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstagramapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstagramapiApplication.class, args);
+	}
+
+}
