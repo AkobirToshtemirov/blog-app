b36e0418d8369f569fd09d4adb33f075b6bc55ca
AkobirToshtemirov <toshtemirovakobir8@gmail.com>
2024-02-21 16:12:22 +0500

    add new methods to repositories

--- src/main/java/com/akobir/instagramapi/repository/SubscribeRepository.java
+++ src/main/java/com/akobir/instagramapi/repository/SubscribeRepository.java
@@ -1,9 +1,18 @@
 package com.akobir.instagramapi.repository;
 
 import com.akobir.instagramapi.entity.Subscribe;
+import com.akobir.instagramapi.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface SubscribeRepository extends JpaRepository<Subscribe, Long> {
+    List<Subscribe> findByFollowerUser(User user);
+
+    List<Subscribe> findByFollowingUser(User user);
+
+    Optional<Subscribe> findByFollowerUserAndFollowingUser(User followerUser, User followingUser);
 }
